31881ac197a42cbe4678e272dec44a80a6fd8716
Filip Stojanović <stojafil@fel.cvut.cz>
2026-03-25 16:43:12 +0100

    Sync add_ppa.sh with stable and unstable versions
    Updated the add_ppa.sh script to use --no-install-recommends for apt-get install and modified rosdep update to include EOL distributions.

--- resources/scripts/add_ppa.sh
+++ resources/scripts/add_ppa.sh
@@ -7,7 +7,7 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: Adding MRS Testing PPA repository"
 
-sudo apt-get -y install curl gpg dpkg-dev
+sudo apt-get --no-install-recommends -y install curl gpg dpkg-dev
 
 ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
 
@@ -17,6 +17,6 @@ sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-testing-preferences 
 sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa-testing/ctu-mrs-$ARCH.list"
 sudo apt-get -y update
 
-rosdep update
+rosdep update --include-eol-distros
 
 echo "$0: Finished adding MRS Testing PPA repository"
