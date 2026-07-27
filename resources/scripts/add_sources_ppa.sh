1f393f40f9da176254c46697e32def1014d3fcf9
Filip Stojanović <stojafil@fel.cvut.cz>
2026-07-27 13:34:32 +0200

    Add space

--- resources/scripts/add_sources_ppa.sh
+++ resources/scripts/add_sources_ppa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa-testing/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa-testing/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
 sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa-testing/ctu-mrs-apt.list"
 sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/preferences.d/ctu-mrs-testing-preferences "https://ctu-mrs.github.io/ppa-testing/ctu-mrs-ppa-preferences.txt"
 
