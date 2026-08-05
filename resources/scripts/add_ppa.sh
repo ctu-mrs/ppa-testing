2e94de7562b979cea75eb5366f24701b36731caa
Filip Stojanović <stojafil@fel.cvut.cz>
2026-08-05 12:29:51 +0200

    Shorten flags

--- resources/scripts/add_ppa.sh
+++ resources/scripts/add_ppa.sh
@@ -7,11 +7,11 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: Adding MRS Testing PPA repository"
 
-sudo apt-get --no-install-recommends -o Acquire::Retries="4" -y install curl gpg dpkg-dev
+sudo apt-get --no-install-recommends -o Acquire::Retries=4 -y install curl gpg dpkg-dev
 
 ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
-sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-connrefused -o /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa-testing/ctu-mrs-$ARCH.list"
-curl -s --compressed --retry 4 --retry-max-time 60 --retry-connrefused https://ctu-mrs.github.io/ppa-testing/add_sources_ppa.sh | bash
+sudo curl -s --compressed --retry 4 --retry-connrefused -o /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa-testing/ctu-mrs-$ARCH.list"
+curl -s --compressed --retry 4 --retry-connrefused https://ctu-mrs.github.io/ppa-testing/add_sources_ppa.sh | bash
 rosdep --include-eol-distros --rosdistro=noetic update
 
 echo "$0: Finished adding MRS Testing PPA repository"
--- resources/scripts/remove_ppa.sh
+++ resources/scripts/remove_ppa.sh
@@ -6,7 +6,7 @@ sudo rm /etc/apt/sources.list.d/ctu-mrs-testing.list
 sudo rm /etc/apt/preferences.d/ctu-mrs-testing-preferences
 sudo rm /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list
 
-sudo apt-get -o Acquire::Retries="4" update
+sudo apt-get -o Acquire::Retries=4 update
 rosdep --include-eol-distros --rosdistro=noetic update
 
 echo "$0: Finished removing MRS Testing PPA repository"
